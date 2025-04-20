b2265dfa3ba9076713f7e799417ccbc5f5acbf88
YoBiya <>
2025-04-20 14:45:36 +0900

    quick-scopeの設定を追加

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'easymotion/vim-easymotion'
+  use 'unblevable/quick-scope'
   use 'jacquesbh/vim-showmarks'
   use 'lambdalisue/fern.vim'
   use 'christianchiarulli/nvcode-color-schemes.vim'
